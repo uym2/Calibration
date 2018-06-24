109018231ff2a08f02682b2fd1cd19b76fd493f1
uym2 <ceciliamaitouyen@gmail.com>
2018-06-24 16:12:44 -0700

    update scriptes

--- date_tree.py
+++ date_tree.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-from calibration_lib import calibrate_with_sampling_time,calibrate_log_opt
+from calibration_lib import calibrate_with_sampling_time,calibrate_log_opt,calibrate_composite_opt
 from dendropy import TreeList
 import argparse
 
@@ -8,9 +8,9 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-i","--input",required=True,help="Input trees")
 parser.add_argument("-s","--samplingTime",required=True,help="Sampling time")
-parser.add_argument("-r","--rootAge",required=True,help="Root age")
+parser.add_argument("-r","--rootAge",required=False,help="Root age")
 parser.add_argument("-t","--timeTree",required=True,help="The output trees with branch lengths in time unit")
-#parser.add_argument("-d","--nodeDate",required=False,help="Output all node dates")
+parser.add_argument("-c","--composite",required=False,action='store_true',help="Do composite optimization. Default: NO")
 
 args = vars(parser.parse_args())
 
@@ -28,14 +28,10 @@ with open(args["samplingTime"],"r") as fin:
 #i = 0
 for tree in myTrees:
     #f = calibrate_with_sampling_time(tree,smpl_times)
-    f = calibrate_log_opt(tree,smpl_times,root_age=rootAge)
-    #fout.write("Tree " + str(i) + "\n")
-    #dates = {}
-    #for node in tree.postorder_node_iter():
-        #label = node.taxon.label if node.is_leaf() else node.label
-        #dates[label] = node.time
-    #for label in sorted(dates.keys()):
-    #    fout.write(label + " " + str(dates[label]) + "\n")    
-    #i += 1
+    if args["composite"]:
+        s = calibrate_composite_opt(tree,smpl_times,root_age=rootAge)
+    else:    
+        s = calibrate_log_opt(tree,smpl_times,root_age=rootAge)
 
 myTrees.write_to_path(args["timeTree"],"newick")
+print("Clock rate: " + str(s))
